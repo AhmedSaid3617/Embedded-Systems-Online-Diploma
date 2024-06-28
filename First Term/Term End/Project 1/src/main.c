d939d83126e191fecc44393f41662af5b9fbefe0
AhmedSaid3617 <said.ahmed3617@gmail.com>
2024-06-29 00:07:35 +0300

    First Term: Project 1 done.

--- First Term/Term End/Project 1/src/main.c
+++ First Term/Term End/Project 1/src/main.c
@@ -1,13 +1,32 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#include "driver/driver.h"
+#include "driver.h"
+#define THRESHOLD 20
 
-int main (){
+int pressure;
+
+int main()
+{
 	GPIO_INITIALIZATION();
+	Set_Alarm_actuator(1);
 	while (1)
 	{
-		//Implement your Design 
-	}
+		pressure = getPressureVal();
+		for (int i = 0; i < 1000; i++)
+		{
+			/* code */
+		}
 
+		if (pressure > THRESHOLD)
+		{
+			Set_Alarm_actuator(0);
+			for (int i = 0; i < 2000000; i++)
+			{
+				/* code */
+			}
+		}
+
+		Set_Alarm_actuator(1);
+	}
 }
